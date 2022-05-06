27b20649d857ac2569d62bd649662b2ffeffbd29
Niladri Sekhar Mukherjee <niladrisekharmukherjee3199@gmail.com>
2022-05-06 11:33:51 +0530

    project2 completed

--- controller/codecontroller.js
+++ controller/codecontroller.js
@@ -13,16 +13,18 @@ let collegeData = async (req, res) => {
       return res
         .status(400)
         .send({ status: false, msg: "College abbreviation name missing" });
-    
+
     if (!data.name.match(/^[a-z]+$/))
       return res
         .status(400)
         .send({ status: false, msg: "Please Enter a valid college Name" });
-        if(data.name) {
-        let validName = await collegeModel.findOne({name:data.name})
-        if(validName)
-          return res.status(400).send({status:false , msg: "College name already exists"})
-        }
+    if (data.name) {
+      let validName = await collegeModel.findOne({ name: data.name });
+      if (validName)
+        return res
+          .status(400)
+          .send({ status: false, msg: "College name already exists" });
+    }
     if (!data.fullName)
       return res.status(400).send({ status: false, msg: "Full name missing" });
 
@@ -31,11 +33,16 @@ let collegeData = async (req, res) => {
         .status(400)
         .send({ status: false, msg: "Please Enter a valid Full Name" });
 
-        if(data.fullName) {
-          let validfullName = await collegeModel.findOne({fullName:data.fullName})
-          if(validfullName){
-            return res.status(400).send({status:false , msg: "College fullname already exists"})}
-          }
+    if (data.fullName) {
+      let validfullName = await collegeModel.findOne({
+        fullName: data.fullName,
+      });
+      if (validfullName) {
+        return res
+          .status(400)
+          .send({ status: false, msg: "College fullname already exists" });
+      }
+    }
     if (!data.logoLink)
       return res
         .status(404)
@@ -51,7 +58,7 @@ let collegeData = async (req, res) => {
 let internData = async (req, res) => {
   try {
     let data = req.body;
-    
+
     if (Object.keys(data).length == 0)
       return res
         .status(404)
@@ -64,30 +71,38 @@ let internData = async (req, res) => {
         .send({ status: false, msg: "Please Enter a valid Intern Name" });
     if (!data.email)
       return res.status(404).send({ status: false, msg: "email missing" });
-     if(data.email){
-       let validEmail= await internModel.findOne({email:data.email})
-       if(validEmail){
-       return res.status(400).send({status:false, msg:"emailId already exists"})}
-     }
-     if (!data.mobile)
+    if (data.email) {
+      let validEmail = await internModel.findOne({ email: data.email });
+      if (validEmail) {
+        return res
+          .status(400)
+          .send({ status: false, msg: "emailId already exists" });
+      }
+    }
+    if (!data.mobile)
       return res
         .status(404)
         .send({ status: false, msg: "Mobile Number missing" });
-        if(data.mobile){
-          let validMobile= await internModel.findOne({mobile:data.mobile})
-          if(validMobile){
-          return res.status(400).send({status:false , msg: "Mobile Number already exists"})}
-        }
-       
+    if (data.mobile) {
+      let validMobile = await internModel.findOne({ mobile: data.mobile });
+      if (validMobile) {
+        return res
+          .status(400)
+          .send({ status: false, msg: "Mobile Number already exists" });
+      }
+    }
 
     if (!data.collegeId)
       return res.status(404).send({ status: false, msg: "CollegeId missing" });
-    
+      if (!/^[0-9a-fA-F]{24}$/.test(data.collegeId)){
+        return  res.status(400).send({status:false, message:"Please enter valid 24 characters in collegeId "})
+       
+    }
+
     let id = data.collegeId;
     let validCollege = await collegeModel.findById(id).catch((err) => null);
     if (!validCollege)
       return res.status(404).send({ status: false, msg: "invalid college id" });
-
 
     let result = await internModel.create(data);
     res.status(201).send({ result });
@@ -98,7 +113,7 @@ let internData = async (req, res) => {
 //=========================================================== GET Api For Colleges=========================================================
 let getCollegeDetails = async (req, res) => {
   try {
-    let data = req.query.name;
+    let data = req.query.collegeName;
 
     if (!data)
       return res
